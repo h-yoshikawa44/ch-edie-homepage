0c5e04f73d0abf208ab45acc9c8edeec31a06940
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-11-22 18:46:02 +0900

    WIP: メニュー作成(#3)
    残：メニューないリンク押下時のメニュークローズ

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
 import '../css/style.css';
 import 'focus-visible';
+import 'wicg-inert';
+
+const contents = document.getElementById('contents');
+document.body.classList;
+
+const menuOpenBtn = document.getElementById('open-menu-btn');
+menuOpenBtn.addEventListener('click', () => {
+  document.getElementById('menu').classList.add('menu__open');
+  contents.setAttribute('inert', true);
+  document.body.classList.add('overflow-hidden');
+});
+
+const menuCloseBtn = document.getElementById('close-menu-btn');
+menuCloseBtn.addEventListener('click', () => {
+  document.getElementById('menu').classList.remove('menu__open');
+  contents.removeAttribute('inert', false);
+  document.body.classList.remove('overflow-hidden');
+});
